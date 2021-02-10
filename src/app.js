14d749993c96c025acd8b86493eaf02e3e9a2089
afify322 <76813264+afify322@users.noreply.github.com>
2021-02-10 16:22:50 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,37 +1,64 @@
 const express = require('express');
+const cluster = require("cluster");
+const totalCPUs = require('os').cpus().length;
+if (cluster.isMaster) {
+    
+  console.log(`Total Number of CPU Counts is ${totalCPUs}`);
 
-const app = express();
-require('dotenv').config();
-const bodyparser = require('body-parser');
-const cors = require('cors');
-const compression = require('compression');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const admin = require('./routes/admin');
-const patient = require('./routes/patient');
-const clerk = require('./routes/clerk');
-const port = process.env.PORT || 3000;
-const { auth } = require('./middleware/auth');
-app.use(morgan('combined'));
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: false }));
-app.use(compression());
-app.use(cors());
-app.use('/clerk', clerk);
-app.use('/patient', patient);
-app.use('/Admin', admin);
-app.use((error, req, res, next) => {
-  const status = error.statusCode || 500;
-  const { message } = error;
-  const { data } = error;
-  res.status(status).json({ message: error, data });
-});
+  for (var i = 0; i < totalCPUs; i++) {
+      cluster.fork();
+  }
+  cluster.on("online", worker => {
+      console.log(`Worker Id is ${worker.id} and PID is ${worker.process.pid}`);
+  });
+  cluster.on("exit", worker => {
+      console.log(`Worker Id ${worker.id} and PID is ${worker.process.pid} is offline`);
+      console.log("Let's fork new worker!");
+      cluster.fork();
+  });
+}
+else {
+  const app = express();
+  require('dotenv').config();
+  const bodyparser = require('body-parser');
+  const cors = require('cors');
+  const compression = require('compression');
+  const mongoose = require('mongoose');
+  const morgan = require('morgan');
+  const admin = require('./routes/admin');
+  const patient = require('./routes/patient');
+  const clerk = require('./routes/clerk');
+  const port = process.env.PORT || 3000;
+  const { auth } = require('./middleware/auth');
+  const {checkimage}= require('./middleware/fileupload')
 
-mongoose.connect(process.env.MONGO_URL,
-  {
-    useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true,
-  }).then((data) => {
-  app.listen(port);
-}).catch((err) => {
-  console.log(err);
-});
+  app.use(morgan('combined'));
+  app.use(bodyparser.urlencoded({ extended: false }));
+  app.use(bodyparser.json());
+  app.use(compression());
+  app.use(cors());
+  app.use("/",(req,res,next)=>{
+    console.log(`Worker Process Id - ${cluster.worker.process.pid} has accepted the request!`);
+    next()
+  })
+  app.use('/clerk', clerk);
+  app.use('/patient', patient);
+  app.use('/Admin', admin);
+  app.use((error, req, res, next) => {
+    const status = error.statusCode || 500;
+    const { message } = error;
+    const { data } = error;
+    res.status(status).json({ message: error, data });
+  });
+  
+  
+
+  mongoose.connect(process.env.MONGO_URL,
+    {
+      useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true,
+    }).then((data) => {
+    app.listen(port);
+  }).catch((err) => {
+    console.log(err);
+  });
+  }
